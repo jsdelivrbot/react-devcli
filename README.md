a476cfe0f516908a3365e4d68fe8022e2048b845
Stefan Cirlig <scirlig@tacitknowledge.com>
2018-07-01 22:37:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-cli-make
+
+## Simple binary for creating scaffolding react components
+
+## Library coming soon, I promise ;)
---
+++ bin/publish.js
@@ -0,0 +1,3 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hey there, howdy. To start exploring the package type react --help'));
